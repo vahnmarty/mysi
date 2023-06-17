df17272aceb4d6a23542797c73e780b41e78d2e4
vahnmarty <vahnmarty@gmail.com>
2023-06-17 18:46:10 +0800

    religion

--- app/Http/Livewire/Application/Forms/ReligionFormTrait.php
+++ app/Http/Livewire/Application/Forms/ReligionFormTrait.php
@@ -13,6 +13,7 @@ use Filament\Forms\Components\TagsInput;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\CheckboxList;
+use Filament\Forms\Components\TextInput\Mask;
 
 trait ReligionFormTrait{
 
@@ -26,7 +27,7 @@ trait ReligionFormTrait{
                 ->lazy()
                 ->required()
                 ->afterStateUpdated(function($state){
-                    $this->autoSave('religion', $state, 'student');
+                    $this->autoSaveStudent('religion', $state);
                 }),
             TextInput::make('student.religion_other')
                 ->label('If "Other," add it here')
@@ -35,25 +36,45 @@ trait ReligionFormTrait{
                 ->placeholder('Enter Religion')
                 ->hidden(fn (Closure $get) => $get('student.religion') !== ReligionType::Other)
                 ->afterStateUpdated(function(Closure $get, $state){
-                    $this->autoSave('religion_other', $state, 'student');
+                    $this->autoSaveStudent('religion_other', $state);
                 }),
             TextInput::make('student.religious_community')
                 ->label('Church/Faith Community')
                 ->lazy()
                 ->afterStateUpdated(function(Closure $get, $state){
-                    $this->autoSave('religious_community', $state, 'student');
+                    $this->autoSaveStudent('religious_community', $state);
                 }),
             TextInput::make('student.religious_community_location')
                 ->label('Church/Faith Community Location')
                 ->lazy()
                 ->afterStateUpdated(function(Closure $get, $state){
-                    $this->autoSave('religious_community_location', $state, 'student');
+                    $this->autoSaveStudent('religious_community_location', $state);
+                }),
+            TextInput::make('student.baptism_year')
+                ->label('Baptism Year')
+                ->numeric()
+                ->integer()
+                ->minLength(4)
+                ->maxLength(4)
+                ->lazy()
+                ->afterStateUpdated(function(Closure $get, $state){
+                    $this->autoSaveStudent('baptism_year', $state);
+                }),
+            TextInput::make('student.confirmation_year')
+                ->label('Confirmation Year')
+                ->numeric()
+                ->integer()
+                ->minLength(4)
+                ->maxLength(4)
+                ->lazy()
+                ->afterStateUpdated(function(Closure $get, $state){
+                    $this->autoSaveStudent('confirmation_year', $state);
                 }),
             
         ];
     }
 
-    private function autoSaveReligion($column, $value)
+    private function autoSaveStudentReligion($column, $value)
     {
         $model = $this->app->student;
         $model->$column = $value;
