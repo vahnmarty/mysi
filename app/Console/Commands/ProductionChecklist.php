5f3e399f6bc4bd4e31bfaa0a86d7d376acade5ac
vahnmarty <vahnmarty@gmail.com>
2023-06-23 14:35:53 +0800

    config

--- app/Console/Commands/ProductionChecklist.php
+++ app/Console/Commands/ProductionChecklist.php
@@ -27,6 +27,8 @@ class ProductionChecklist extends Command
     {
         $this->newLine(3);  
         
+        $this->checkConfigs();
+        $this->newLine();
         $this->checkSettings();
 
         $this->newLine(3);
@@ -42,6 +44,28 @@ class ProductionChecklist extends Command
         foreach($array as $arr)
         {
             $this->validateSetting($arr);
+        }
+    }
+
+    public function checkConfigs()
+    {
+        $this->line('Config/Environment');
+        $this->newLine();
+
+        $array = ['settings.si.admissions.email', 'settings.payment.application_fee', 'services.authorize.login_id', 'services.authorize.transaction_key'];
+
+        foreach($array as $path)
+        {
+            $this->validateConfig($path);
+        }
+    }
+
+    public function validateConfig($path)
+    {
+        if(config($path)){
+            $this->info($this->getSuccessIcon() . " {$path}: " . config($path));
+        }else{
+            $this->error($this->getFailedIcon() . " {$path}: null" );
         }
     }
 
