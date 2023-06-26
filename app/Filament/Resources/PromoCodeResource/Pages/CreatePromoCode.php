494a9f298ab441ded1e67f16e215cf3b869aeeed
vahnmarty <vahnmarty@gmail.com>
2023-06-26 16:45:48 +0800

    promo code

---
+++ app/Filament/Resources/PromoCodeResource/Pages/CreatePromoCode.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\PromoCodeResource\Pages;
+
+use App\Filament\Resources\PromoCodeResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreatePromoCode extends CreateRecord
+{
+    protected static string $resource = PromoCodeResource::class;
+}
