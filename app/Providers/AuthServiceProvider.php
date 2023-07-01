29dd8baf50214e974c8a701ad5f7482a0a40b926
vahnmarty <vahnmarty@gmail.com>
2023-07-01 23:55:44 +0800

    reset password

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,8 +5,11 @@ namespace App\Providers;
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\HtmlString;
 use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+use Illuminate\Support\Facades\Lang;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -30,6 +33,17 @@ class AuthServiceProvider extends ServiceProvider
                 ->subject('Verify Email Address')
                 ->line('Click the button below to verify your email address.')
                 ->action('Verify Email Address', $url)
+                ->salutation(new HtmlString("**Regards**, <br>" . config('app.name')));
+        });
+
+        ResetPassword::toMailUsing(function (object $notifiable, string $url) {
+            return (new MailMessage)
+                ->greeting('Hello, ' . $notifiable->first_name)
+                ->subject('Reset MySI Portal Password')
+                ->line(Lang::get('You are receiving this email because we received a password reset request for your account.'))
+                ->action(Lang::get('Reset Password'), $url)
+                ->line(Lang::get('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')]))
+                ->line(Lang::get('If you did not request a password reset, no further action is required.'))
                 ->salutation(new HtmlString("**Regards**, <br>" . config('app.name')));
         });
     }
