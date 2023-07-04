4b0c98bfcc4bdd942933831b41f41b01f6f7432e
vahnmarty <vahnmarty@gmail.com>
2023-07-05 00:43:07 +0800

    removed grade names

--- app/Enums/GradeLevel.php
+++ app/Enums/GradeLevel.php
@@ -17,18 +17,18 @@ final class GradeLevel extends Enum
     // Dropdown options are:  Pre-kindergarten, Kindergarten, 1, 2, 3, 4, 5, 6, 7, 8, Freshman, Sophomore, Junior, Senior, College, Post HS/College
     const PreKindergarten = 'Pre-kindergarten';
     const Kindergarten = 'Kindergarten';
-    const Grade1 = 'Grade 1';
-    const Grade2 = 'Grade 2';
-    const Grade3 = 'Grade 3';
-    const Grade4 = 'Grade 4';
-    const Grade5 = 'Grade 5';
-    const Grade6 = 'Grade 6';
-    const Grade7 = 'Grade 7';
-    const Grade8 = 'Grade 8';
-    const Freshman = 'Freshman';
-    const Sophomore = 'Sophomore';
-    const Junior = 'Junior';
-    const Senior = 'Senior';
+    const Grade1 = '1';
+    const Grade2 = '2';
+    const Grade3 = '3';
+    const Grade4 = '4';
+    const Grade5 = '5';
+    const Grade6 = '6';
+    const Grade7 = '7';
+    const Grade8 = '8';
+    const Freshman = '9`';
+    const Sophomore = '10`';
+    const Junior = '11';
+    const Senior = '12';
     const College = 'College';
     const PostCollege = 'Post HS/College';
 
