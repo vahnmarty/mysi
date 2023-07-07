95bcb554dbe8fb36ddc5afc3a8df37e0cf8a2f14
vahnmarty <vahnmarty@gmail.com>
2023-07-08 00:24:56 +0800

    textarea parent statement

--- app/Http/Livewire/Application/Forms/ParentStatementTrait.php
+++ app/Http/Livewire/Application/Forms/ParentStatementTrait.php
@@ -10,19 +10,22 @@ use App\Enums\ParentType;
 use App\Enums\RacialType;
 use App\Enums\CommonOption;
 use App\Enums\ReligionType;
+use App\Rules\MaxWordCount;
 use Illuminate\Support\HtmlString;
+use Livewire\Component as Livewire;
 use Filament\Forms\Components\Radio;
 use App\Enums\FamilySpiritualityType;
-use Filament\Forms\Components\Select;
 //use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\Select;
+use App\Forms\Components\WordTextArea;
 use Filament\Forms\Components\Fieldset;
 use Filament\Forms\Components\TagsInput;
 use Filament\Forms\Components\TextInput;
 use Filament\Notifications\Notification;
+
+
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\Placeholder;
-
-
 use Filament\Forms\Components\CheckboxList;
 use Filament\Forms\Components\TextInput\Mask;
 use Wiebenieuwenhuis\FilamentCharCounter\Textarea;
@@ -35,34 +38,46 @@ trait ParentStatementTrait{
             Placeholder::make('section_parent_statement')
                 ->label('')
                 ->content(new HtmlString('*This section is to be completed by a parent/guardian only.')),
-            Textarea::make('why_si_for_your_child')
+            WordTextArea::make('why_si_for_your_child')
                 ->label("Why do you want your child to attend St. Ignatius College Preparatory?")
-                ->helperText("Up to 500 characters only.")
-                ->lazy()
+                ->helperText("Please limit your answer to 75 words.")
                 ->required()
                 ->rows(5)
-                ->maxLength(500)
-                ->afterStateUpdated(function($state){
+                ->lazy()
+                ->wordLimit(75)
+                ->rules([
+                    new MaxWordCount(75)
+                ])
+                ->afterStateUpdated(function(Livewire $livewire, WordTextArea $component, Closure $get, $state){
+                    $livewire->validateOnly($component->getStatePath());
                     $this->autoSave('why_si_for_your_child', $state);
                 }),
-            Textarea::make('childs_quality_and_area_of_growth')
+            WordTextArea::make('childs_quality_and_area_of_growth')
                 ->label("Explain your child's most endearing quality and an area of growth.")
-                ->helperText("Up to 500 characters only.")
+                ->helperText("Please limit your answer to 75 words.")
                 ->lazy()
                 ->required()
                 ->rows(5)
-                ->maxLength(500)
-                ->afterStateUpdated(function($state){
+                ->wordLimit(75)
+                ->rules([
+                    new MaxWordCount(75)
+                ])
+                ->afterStateUpdated(function(Livewire $livewire, WordTextArea $component, Closure $get, $state){
+                    $livewire->validateOnly($component->getStatePath());
                     $this->autoSave('childs_quality_and_area_of_growth', $state);
                 }),
-            Textarea::make('something_about_child')
+            WordTextArea::make('something_about_child')
                 ->label("Please tell us something about your child that does not appear on this application.")
-                ->helperText("Up to 500 characters only.")
+                ->helperText("Please limit your answer to 75 words.")
                 ->lazy()
                 ->required()
                 ->rows(5)
-                ->maxLength(500)
-                ->afterStateUpdated(function($state){
+                ->wordLimit(75)
+                ->rules([
+                    new MaxWordCount(75)
+                ])
+                ->afterStateUpdated(function(Livewire $livewire, WordTextArea $component, Closure $get, $state){
+                    $livewire->validateOnly($component->getStatePath());
                     $this->autoSave('something_about_child', $state);
                 }),
             TextInput::make('parent_statement_by')
