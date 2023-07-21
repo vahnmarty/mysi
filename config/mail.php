59f3b114d56ff4b0cd123c47e79b2891fa83eb48
vahnmarty <vahnmarty@gmail.com>
2023-07-21 23:24:24 +0800

    mail config

--- config/mail.php
+++ config/mail.php
@@ -45,6 +45,14 @@ return [
             'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN'),
             'auth_mode'  => null,
+            'verify_peer' => false,
+            'stream'     => [
+                'ssl' => [
+                    'allow_self_signed' => true,
+                    'verify_peer'       => false,   // <-------------- this is the important part 
+                    'verify_peer_name'  => false,
+                ],
+            ],
         ],
 
         'ses' => [
