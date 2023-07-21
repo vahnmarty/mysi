39702d2e0a6010f0a9b4e8cba091c70c70cae5c2
vahnmarty <vahnmarty@gmail.com>
2023-07-21 09:52:27 +0800

    stream config mail

--- config/mail.php
+++ config/mail.php
@@ -85,6 +85,14 @@ return [
                 'log',
             ],
         ],
+
+        'stream' => [
+            'ssl' => [
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+                'allow_self_signed' => true,
+            ],
+        ],
     ],
 
     /*
