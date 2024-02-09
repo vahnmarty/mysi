ef89e18c426e9be48c5fc2ce8f998005ba8f50b6
vahnmarty <vahnmarty@gmail.com>
2024-02-09 23:40:21 +0800

    survey form

--- app/Http/Livewire/SurveyForm.php
+++ app/Http/Livewire/SurveyForm.php
@@ -3,11 +3,113 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Application;
+use App\Models\NotificationMessage;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Contracts\HasForms;
+use Filament\Forms\Components\Fieldset;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Placeholder;
+use Filament\Forms\Concerns\InteractsWithForms;
+use Awcodes\FilamentTableRepeater\Components\TableRepeater;
 
-class SurveyForm extends Component
+class SurveyForm extends Component implements HasForms
 {
+    use InteractsWithForms;
+    
+    public $type;
+
+    public $data = [];
+    
     public function render()
     {
-        return view('livewire.survey-form');
+        return view('livewire.survey-form')
+            ->layoutData(['title' => 'Survey']);
+    }
+
+    public function mount($uuid)
+    {
+        $notificationMessage = NotificationMessage::with('application')->whereUuid($uuid)->firstOrFail();
+
+        $app = $notificationMessage->application;
+
+        $this->type = $app->accepted() ? 'Accepted' : 'Declined';
+
+        $this->form->fill();
+    }
+
+    protected function getFormStatePath(): string 
+    {
+        return 'data';
+    }
+
+    protected function getFormSchema()
+    {
+        return [
+            TableRepeater::make('survey_schools')
+                ->label('')
+                ->defaultItems(4)
+                ->hideLabels()
+                ->createItemButtonLabel('Add')
+                ->columnSpan('full')
+                ->schema([
+                    TextInput::make('school_name')
+                        ->label('Name of School')
+                        ->required(),
+                    Select::make('school_decision')
+                        ->label("School's Decision")
+                        ->placeholder('Select')
+                        ->options([])
+                        ->required(),
+                    Select::make('applied_for_aid')
+                        ->label("Applied for Aid")
+                        ->placeholder('Select')
+                        ->options([])
+                        ->required(),
+                    TextInput::make('amount_aid')
+                        ->label("Amount of Aid or Scholarship Offered")
+                        ->prefix('$'),
+                    TextInput::make('comment')
+                        ->label("Comment")
+                ]),
+            Fieldset::make('Rank the three(3) most important reasons for choosing SI:')   
+                ->columns(2)
+                ->schema([
+                    Select::make('most_important_reason')
+                        ->label('Most Important Reason')
+                        ->inlineLabel()
+                        ->options([]),
+                    TextInput::make('most_important_reason_comment')
+                        ->label('Comment')
+                        ->inlineLabel(),
+                    Select::make('second_important_reason')
+                        ->label('Second Important Reason')
+                        ->inlineLabel()
+                        ->options([]),
+                    TextInput::make('second_important_reason_comment')
+                        ->label('Comment')
+                        ->inlineLabel(),
+                    Select::make('third_important_reason')
+                        ->label('Third Important Reason')
+                        ->inlineLabel()
+                        ->options([]),
+                    TextInput::make('third_important_reason_comment')
+                        ->label('Comment')
+                        ->inlineLabel(),
+                ]),
+            Textarea::make('student_visit_program_remarks')
+                ->label('If you attended a Student Visit Program or any other Admissions event, we would appreciate any comments regarding your experience:'),
+            Textarea::make('si_admission_process')
+                ->label('Please let us know your thoughts regarding the SI admissions process this year:')
+        ];
+    }
+
+    public function submit()
+    {
+        $data = $this->form->getState();
+
+        dd($data);
     }
 }
--- resources/views/livewire/survey-form.blade.php
+++ resources/views/livewire/survey-form.blade.php
@@ -1,3 +1,30 @@
 <div>
-    {{-- In work, do what you enjoy. --}}
+    <div class="flex justify-between">
+        @if($type == 'Accepted')
+        <h2 class="text-2xl font-semibold font-heading text-primary-blue">
+            Enroll at St. Ignatius College Preparatory Survey
+        </h2>
+        @else
+        <h2 class="text-2xl font-semibold font-heading text-primary-blue">
+            Decline Acceptance at St. Ignatius College Preparatory Survey
+        </h2>
+        @endif
+    </div>
+
+
+    <div class="pb-32 mt-8">
+
+        <p>Please answer the following questions to assist us in evaluating our admissions process and to complete your registration at St. Ignatius College Preparatory.</p>
+        <p class="mt-8 mb-8 font-bold">
+            Please list, in order of preference, the schools to which you applied , the admission decision (accepted/waitlisted/not accepted), and Financial Aid or scholarship information, if applicable:
+        </p>
+        <form wire:submit.prevent="submit">
+
+            {{ $this->form }}
+
+            <div class="flex justify-center mt-8">
+                <button type="submit" class="btn-primary">Submit</button>
+            </div>
+        </form>
+    </div>
 </div>
