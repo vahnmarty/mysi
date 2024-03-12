832820735f38284bbf620b7e01a8460838e3520a
vahnmarty <vahnmarty@gmail.com>
2024-03-12 15:28:04 +0800

    text center

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -8,7 +8,7 @@
     <img src="{{ asset('img/error.jpg') }}" class="w-64 h-auto">
     
     <h3 class="mt-8 text-2xl font-bold">We're under maintenance.</h3>
-    <p class="mt-4 text-sm text-gray-700">
+    <p class="mt-4 text-sm text-center text-gray-700">
         The system is currently unavailable. We are currently working on preparing it for Notifications and Registration.<br> Please come back on Friday, March 15 at 4 pm PDT.
     </p>
 </div>
