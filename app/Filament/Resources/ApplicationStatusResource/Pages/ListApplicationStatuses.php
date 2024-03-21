747ca3226f267295f1bbd4398b0bda046e34b0a2
vahnmarty <vahnmarty@gmail.com>
2024-03-21 22:36:02 +0800

    sort name

--- app/Filament/Resources/ApplicationStatusResource/Pages/ListApplicationStatuses.php
+++ app/Filament/Resources/ApplicationStatusResource/Pages/ListApplicationStatuses.php
@@ -2,13 +2,20 @@
 
 namespace App\Filament\Resources\ApplicationStatusResource\Pages;
 
-use App\Filament\Resources\ApplicationStatusResource;
 use Filament\Pages\Actions;
+use App\Models\ApplicationStatus;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\ApplicationStatusResource;
 
 class ListApplicationStatuses extends ListRecords
 {
     protected static string $resource = ApplicationStatusResource::class;
+
+    protected function getTableQuery(): Builder
+    {
+        return ApplicationStatus::join('applications', 'application_status.application_id', 'applications.id');
+    }
 
     protected function getActions(): array
     {
