3dcec89d0bc6fdcaa9ac668c65e6d05a31bd37ea
a2server <rferro@siprep.org>
2024-07-01 11:51:42 -0400

    503 updated by ramil

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -9,8 +9,8 @@
     
     <h3 class="mt-8 text-2xl font-bold">We're under maintenance.</h3>
     <p class="mt-4 text-sm text-center text-gray-700">
-        The MySI Portal is currently under system maintenance. It will be back up on Monday, July 1, 2024 at 8 am PT.<br><br>
-        For the Class of 2028, if you need to upload your SFUSD Health Form, please email the documents along with the student name to <A HREF = "mailto:mysi_admin@siprep.org?subject=SFUSD Health Form"><FONT COLOR = "blue">mysi_admin@siprep.org</FONT></A>.
+        The MySI Portal is currently under system maintenance. It will be back up on Monday, July 1, 2024 at 10 am PT.<br><br>
+        For the Class of 2028, click on the link to download the <A HREF = "/files/SIFreshmanHealthForm.pdf"><FONT COLOR = "blue">SFUSD Health Form</FONT></A>.  Send the completed form, with the doctor's signature, to <A HREF = "mailto:mysi_admin@siprep.org?subject=SFUSD Health Form"><FONT COLOR = "blue">mysi_admin@siprep.org</FONT></A> along with the student's name.
     </p>
 </div>
 @endsection
