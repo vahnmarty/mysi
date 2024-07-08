fd70406b343d0c829a3b32709e1ed18c79c2d3df
vahnmarty <vahnmarty@gmail.com>
2024-07-09 00:33:51 +0800

    disabeld parent creation/edit

--- resources/views/livewire/application/parent-information.blade.php
+++ resources/views/livewire/application/parent-information.blade.php
@@ -7,11 +7,13 @@
         {{ $this->table }}
     </div>
 
+    @if(false)
     <div class="py-6">
         <button x-data="{ form: $wire.entangle('enable_form') }" 
             x-show="!form" 
             class="btn-primary-red" wire:click="add">Add</button>
     </div>
+    @endif
 
 
     <div x-data="{ enable: $wire.entangle('enable_form') }" 
@@ -26,10 +28,12 @@
             
             {{ $this->form }}
 
+            @if(false)
             <div class="flex justify-end gap-8 mt-8">
                 <button type="button" wire:click="cancel()" class="btn-primary">Cancel</button>
                 <button type="submit" class="btn-primary">Save Changes</button>
             </div>
+            @endif
         </form>
     </div>
 </div>
